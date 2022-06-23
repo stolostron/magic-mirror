e9db3d629838a22250f76c41cb5562b037e19259
mprahl <mprahl@users.noreply.github.com>
2022-06-23 13:12:03 -0400

    Use git cherry-pick instead of patch files
    This allows the option of `-x` in `git cherry-pick` so that the original
    upstream commit hash is recorded in the forked repo's commit message.
    
    This will allow easy searching of an upstream commit in the fork.
    
    Signed-off-by: mprahl <mprahl@users.noreply.github.com>

--- src/git.ts
+++ src/git.ts
@@ -4,56 +4,58 @@ import path from "path";
 import simpleGit from "simple-git";
 import tmp from "tmp";
 
+// patchLocation represents the head (commit hash of the start of the patch) and the number of commits as part of the
+// patch.
+export type patchLocation = {
+  head: string;
+  numCommits: number;
+};
+
 /**
- * Apply the input Git patches and push the changes.
+ * Apply the input Git patches using `git cherry-pick` and push the changes.
  * @param {string} remoteURL the Git URL to clone from and push to. It should include any required authentication.
+ * @param {string} upstreamRemoteURL the upstream Git URL to the repo to cherry-pick from.
  * @param {string} sourceBranch the branch to use as a base for applying the patches.
  * @param {string} targetBranch the branch to push applied patches to.
- * @param {Array<string>} patches the array of Git patch strings to apply.
- * @return {Promise<boolean>} a promise that resolves to a boolean determining if the patches actually affected the Git
- *   history and were pushed. This will be false if the patches are diffs that are already applied on the target branch.
- *   This rejects when there is a Git error such as a patch not applying.
+ * @param {Array<patchLocation>} patchLocations the array of patch locations (i.e. patch commit HEAD and number of
+ *   commits) to apply.
+ * @return {Promise<void>} a promise that resolves to nothing.
  */
 export async function applyPatches(
   remoteURL: string,
+  upstreamRemoteURL: string,
   sourceBranch: string,
   targetBranch: string,
-  patches: Array<string>,
-): Promise<boolean> {
-  if (!patches.length) {
+  patchLocations: Array<patchLocation>,
+) {
+  if (!patchLocations.length) {
     throw new Error("One or more patches are required");
   }
 
   const dirObj = tmp.dirSync({ keep: true, unsafeCleanup: true });
   try {
-    const patchFiles: Array<string> = [];
-    patches.map((patch, i) => {
-      if (patch) {
-        const patchPath = path.join(dirObj.name, `patch${i + 1}.patch`);
-        fs.writeFileSync(patchPath, patches[i]);
-        patchFiles.push(patchPath);
-      }
-    });
-
-    const gitPath = path.join(dirObj.name, "repo");
-    fs.mkdirSync(gitPath);
     // These method calls, when chained together, will run serially. Any failure will cause
     // the chain to stop.
-    const git = simpleGit(gitPath);
-    await git.clone(remoteURL, gitPath).checkoutBranch(targetBranch, `origin/${sourceBranch}`);
+    const git = simpleGit(dirObj.name);
+    await git
+      .clone(remoteURL, dirObj.name)
+      .checkoutBranch(targetBranch, `origin/${sourceBranch}`)
+      .remote(["add", "upstream", upstreamRemoteURL])
+      .fetch(["upstream", "--prune"]);
 
-    const originalHead = await git.revparse(["HEAD"]);
-
-    await Promise.all(patchFiles.map((patchFile) => git.raw(["am", "-3", patchFile])));
-
-    const newHead = await git.revparse(["HEAD"]);
-    if (originalHead === newHead) {
-      return false;
-    }
+    await Promise.all(
+      patchLocations.map((p) =>
+        git.raw([
+          "cherry-pick",
+          "-x",
+          `${p.head}~${p.numCommits}..${p.head}`,
+          "--allow-empty",
+          "--keep-redundant-commits",
+        ]),
+      ),
+    );
 
     await git.push(["origin", "HEAD"]);
-
-    return true;
   } finally {
     dirObj.removeCallback();
   }
