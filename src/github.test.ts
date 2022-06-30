901461e6f6792dee4ab63c4102690017bc2623c6
Dale Haiducek <dhaiduce@redhat.com>
2022-06-30 18:12:54 -0400

    Output `cherry-pick` command in issue
    Signed-off-by: Dale Haiducek <dhaiduce@redhat.com>

--- src/github.test.ts
+++ src/github.test.ts
@@ -68,3 +68,40 @@ test("createFailureIssue with prID", () => {
   };
   expect(mockClient.issues.create).toHaveBeenCalledWith(expectedIssueArgs);
 });
+
+test("createFailureIssue with patchCmd", () => {
+  const mockClient = {
+    issues: {
+      create: jest.fn(() => {
+        return new Promise((resolve) => {
+          resolve({
+            data: { number: 6 },
+          });
+        });
+      }),
+    },
+  };
+
+  expect(
+    createFailureIssue(
+      // @ts-ignore
+      mockClient, "kramerica", "industries", "upstream", "main", [5, 7], "it failed", undefined, ["git cherry-pick"],
+    ),
+  ).resolves.toEqual(6);
+
+  expect(mockClient.issues.create.mock.calls.length).toEqual(1);
+
+  const expectedIssueArgs = {
+    body:
+      "🪞 Magic Mirror 🪞 failed to sync the following upstream pull-requests because it failed:\n" +
+      "* upstream/industries#5\n* upstream/industries#7\n\n" +
+      "Syncing is paused for the branch main on kramerica/industries until the issue is manually resolved and this " +
+      "issue is closed.\n" +
+      "\nCommands to recreate the issue:\n\n```\ngit cherry-pick\n```\n" +
+      "\n![sad Yoda](https://media.giphy.com/media/3o7qDK5J5Uerg3atJ6/giphy.gif)",
+    owner: "kramerica",
+    repo: "industries",
+    title: "😿 Failed to sync the upstream PRs: #5, #7",
+  };
+  expect(mockClient.issues.create).toHaveBeenCalledWith(expectedIssueArgs);
+});
